a3436abc18e06221bcb309b6aead61c68bc879f9
whitelawcole <153958466+whitelawcole@users.noreply.github.com>
2024-05-02 11:24:04 -0700

    Update Golf_app.py

--- Golf_app.py
+++ Golf_app.py
@@ -1,18 +1,18 @@
 import streamlit as st
 import sqlite3
-#import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 import pandas as pd
-#import seaborn as sns 
-#from sklearn.preprocessing import StandardScaler
+import seaborn as sns 
+from sklearn.preprocessing import StandardScaler
 import numpy as np
-#from sklearn.model_selection import train_test_split
-#from sklearn.linear_model import LinearRegression
-#from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
-#from sklearn.preprocessing import StandardScaler
-#from sklearn.decomposition import PCA
-#from sklearn.cluster import KMeans 
-#import mplcursors
-#import plotly.express as px
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
+from sklearn.preprocessing import StandardScaler
+from sklearn.decomposition import PCA
+from sklearn.cluster import KMeans 
+import mplcursors
+import plotly.express as px
 
 
 conn = sqlite3.connect('Golf_data.db')
