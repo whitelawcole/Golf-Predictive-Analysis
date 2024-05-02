9ec65f0038892c4e3b1198efbace96efd5222d04
whitelawcole <153958466+whitelawcole@users.noreply.github.com>
2024-05-02 11:21:01 -0700

    Update Golf_app.py

--- Golf_app.py
+++ Golf_app.py
@@ -3,7 +3,7 @@ import sqlite3
 #import matplotlib.pyplot as plt
 import pandas as pd
 #import seaborn as sns 
-from sklearn.preprocessing import StandardScaler
+#from sklearn.preprocessing import StandardScaler
 import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
