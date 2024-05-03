8d164ce9fe9cc020e372b5244a588b2f29fb072f
whitelawcole <153958466+whitelawcole@users.noreply.github.com>
2024-05-02 21:42:34 -0700

    Update Golf_app.py

--- Golf_app.py
+++ Golf_app.py
@@ -11,6 +11,7 @@ from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
 from sklearn.preprocessing import StandardScaler
 from sklearn.decomposition import PCA
 from sklearn.cluster import KMeans 
+import mplcursors
 import plotly.express as px
 
 
@@ -62,6 +63,8 @@ def visualize_clusters(data, clusters, title):
     fig_plotly.update_layout(plot_bgcolor='white')
     # Display Plotly figure in Streamlit
     st.plotly_chart(fig_plotly)
+
+
 def box_plot_cluster():
     # Group the DataFrame by cluster label
     cluster_groups = cluster_df.groupby('kmeans_2')
@@ -264,7 +267,7 @@ def main():
     st.title('PGA Data Exploration Project')
 
     # Add navigation to sidebar
-    page = st.sidebar.selectbox("Choose a page", ["Main Page", "Statistical Analysis", "Data Exploration"])
+    page = st.sidebar.selectbox("Choose a page", ["Main Page", "Statistical Analysis", "Data Exploration", "Data Set Description", "Questions"])
 
     # Render different pages based on user selection
     if page == "Main Page":
@@ -273,53 +276,74 @@ def main():
         render_stats_page()
     elif page == "Data Exploration":
         render_data_page()
+    elif page == 'Data Set Description':
+        render_description_page()
+    elif page == 'Questions':
+        render_conclusion_page()
 
 def render_main_page():
     st.header('Main Page')
     st.write('Questions/Answers')
     st.markdown('''
         Cole Whitelaw
+                
+        2. An explanation of how to use your webapp: what interactivity there is, what the plots/charts mean, what your conclusions were, etc
         
-        This web app allows you to query through specific PGA Tournaments and find the winner for that year, the course and course statistics for that year and winner statistics for that year. This will be on the
-        Data Exploration page. The statistical analysis is explained on the 
+        Answer:
+        This web app is a presentation of PGA tour data for the years of 2013 through 2023. The data exploration page allows you to query through specific tournamnets and the year they were played. 
+        After the tournament and year is selected, the app will present the course it was played on along with its statistics, and the winner of that tournament along with the winners statistics for that year. 
+        The statistical analysis was ran to find which player statistics have the largest impact on specific types of courses. The analysis uses a kmeans clustering algorithm to group the courses and a multiple linear regression
+        analysis to find which player statistics have the largest impacts on course cluster wins. As of now it seems like there is not much correlation to player statistics and course cluster wins, this being shown on the statistical analysis page. 
+        A better description of the analysis and its specific componenets can be explained on the Statistical analysis page.  
 
         3. Any major “gotchas” (i.e. things that don’t work, go slowly, could be improved, etc.)
+        
+        Answer:
+        Unfortunately, the analysis did not come to any major conclusions as to what statistics have the largest impact, even though there were some statistics that had a larger impact than others it was not significant. The analysis was ran for each year and each year had different results. 
+        It may have been beneficial to run this analysis for all the years which may have presented a better conclusion but it also may have been worse. The data seems to be a lot better in the current
+        years versus previous which may affect the this analysis in the later years presented. Also, the clustering of the courses is by no means perfect, there is not a clear cut distinction between the groups of courses. It also could have been beneficial 
+        to run a multiple linear regression on each course instead of using clusters but again the varaince could have been worse. 
 
-        In addition, you’ll need to answer the following questions (they’ll go in a specific page in your webapp.  Don’t worry, we’ll learn how to do that!)
-
-        1. What did you set out to study?  (i.e. what was the point of your project?  This should be close to your Milestone 1 assignment, but if you switched gears or changed things, note it here.)
-
-        2. What did you Discover/what were your conclusions (i.e. what were your findings?  Were your original assumptions confirmed, etc.?)
-
-        3. What difficulties did you have in completing the project?
-
-        4. What skills did you wish you had while you were doing the project?
-
-        5. What would you do “next” to expand or augment the project?
+        
     '''
     )
 
 def render_stats_page():
     st.header('Statistical Analysis')
-    st.write('''This analysis used a kmeans clusterig algorithm to group each course into a cluster, based on the analyis 2 clusters were the most accurate (Cluster 0 & Cluster 1). 
+    st.write('''This analysis uses a kmeans clusterig algorithm to group each course into a cluster, based on the analyis 2 clusters was the most reasonable (Cluster 0 & Cluster 1, see elbow plot). 
              Based on the statistics cluster 0 courses seemed to be made up of shorter courses but harder skill play while cluster 1 courses were longer courses but easier skill play. After the clustering was done and added into the data base 
-             I ran a multiple linear regression against the number of wins on a specific cluster based on the player statistics. In the drop down menu below you can select a year and course cluster. This will show how much the course cluster wins 
-             were explained by player statistics. The bar graph will show which specific statistics hold the most weight for that year and course cluster. 
+             a multiple linear regression was used to compare the number of wins on a specific cluster against the player statistics. In the drop down menu below you can select a year and course cluster. This will show how much the course cluster wins 
+             were explained by player statistics and the variance explained of each stat. The bar graph will show which specific statistics hold the most weight for that year and course cluster. 
              
              ''')
-    
-    optimise_k_means(cluster_df[columns], 10)
+    st.write('''
+             
 
+
+    The elbow plot below helps in selecting the optimal number of clusters for K-means clustering. A good clustering solution strikes a balance between having enough clusters to capture the underlying structure of the data without overfitting. The elbow point provides a heuristic for choosing the number of clusters that best represents the inherent patterns in the data.
+    ''')
+    optimise_k_means(cluster_df[columns], 10)
+    
     kmeans = KMeans(n_clusters=2)
     kmeans.fit(cluster_df[columns])
     cluster_df['kmeans_2'] = kmeans.labels_
-
+    st.write('''
+    This graph below provides a visual representation of data points in a two-dimensional space, with clusters differentiated by color.
+    The graph is also interactave so you can see which course falls within each cluster by putting your cursor over a point on the graph. 
+             ''')
     visualize_clusters(principal_components, kmeans.labels_, 'KMeans Clustering with 2 clusters')
+
+    st.write('''
+    This graph below shows how the courses were clustered, the statistics labeled in the middle and the bar graph respresenting the mean of those statistics. 
+             ''')
     box_plot_cluster()
 
 
 
-
+    st.write('''
+    Here a multiple linear regression analysis will be ran based on the year and cluster you choose. The data will present with the variance explained of each statistic along with Absolute error, R2 Value, and Prediction Magnitude. 
+    The bar graph plots each statistic and its variance explained showing which statistics had the largest impact on that year specific to the cluster you choose. 
+             ''')
     year_options = [2023,2022,2021,2020,2019,2018,2017,2016]
     cluster_options = [0,1]
     selected_year = st.selectbox('Select Year', year_options)
@@ -367,6 +391,66 @@ def render_data_page():
     st.write(winner_info)
     st.subheader('Winner Stats')
     st.write(winner_stats)
+
+def render_description_page():
+    st.header('Data Set Description')
+    st.write('Description of Data Sets Used')
+    st.markdown('''
+
+        Data Set 1: Sportradar API: https://developer.sportradar.com/golf/reference/golf-overview
+        This API returns all PGA tour golfers statistics per year, below is an example of one player in 2024 from the API endpoint. This is for Seung-Yul Noh in 2024. [{"id":"7d470d98-95a1-42a5-b5eb-39bd2259ac5f","first_name":"Seung-Yul","last_name":"Noh","country":"SOUTH KOREA","abbr_name":"S.Noh","statistics":{"events_played":1,"first_place":0,"second_place":0,"third_place":0,"top_10":0,"top_25":0,"cuts":0,"cuts_made":1,"withdrawals":0,"points":12.0,"points_rank":185,"earnings":21480.0,"earnings_rank":190,"drive_avg":293.6,"drive_acc":60.71,"gir_pct":72.22,"putt_avg":1.731,"sand_saves_pct":50.0,"birdies_per_round":4.25,"world_rank":538,"scrambling_pct":75.0,"scoring_avg":71.225,"total_driving":1998}}
+
+        Data set 2: pgatour.com: https://www.pgatour.com/schedule/2024 
+        For this data source I scraped all the past PGA tournament courses and winners. The data is stored in the HTML so I scraped the data using BeautifulSoup. 
+
+        Data Set 3: https://datagolf.com/course-table
+        The last data source was a CSV file exported from Datagolf.com. This csv file contains statistics of most of the golf courses on the PGA tour (some courses are missing). Examples of variables include: par, total yardage, total scoring average, etc. 
+
+
+        Note: For the first two data sets the data was collected for the years of 2013-2023
+                
+        The data collected from these data sets was combined into a sqlite relational data base. The model for this data base is located in the github repository.
+
+        
+    '''
+    )
+
+
+
+
+
+
+def render_conclusion_page():
+    st.header('Questions')
+    st.write('Conlcuding Questions')
+    st.markdown('''
+
+        1. What did you set out to study?  (i.e. what was the point of your project?  This should be close to your Milestone 1 assignment, but if you switched gears or changed things, note it here.)
+        
+            Answer: I set out to study if specific types of golfers tend to do better on specific types of courses. Are there certain skills/statistics that enable players to win more on certain courses based on the course statistics? 
+
+        2. What did you Discover/what were your conclusions (i.e. what were your findings?  Were your original assumptions confirmed, etc.?)
+
+            Answer: Based on the data I had there was not a clear answer as to which players did well on what courses. 
+            The correlation was weak and it changed every year, maybe pointing to the fact that there is no correlation. More data or a different analysis may change this conclusion but more research needs to be done. 
+            I thought there would be a stornger correlation but this was not the case, the correlation was also very variable between each year and cluster.       
+
+        3. What difficulties did you have in completing the project?
+            
+            Answer: Finding all the data and scraping it was the most difficult part of the project. Ensuring that the data was collected and inputted 
+            correctly into the data based was a challenge because making a small mistake could corupt the entire data base. It was also not easy to scrape from PGA.com, the html was very messy and unorganized making it difficult to structure the data in order to properly input it into my database.  
+
+        4. What skills did you wish you had while you were doing the project?
+        
+            Answer: It would have been extremely beneficial having a more complete understanding of building relational data bases and querying them in SQL. 
+            I ran into some difficulty with joining tables and querying information, which I solved by just creating a pandas dataframe from the database tables.
+            The web app would be more complete if the data was complied directly from the sqlite databse but I ran into some difficulties doing so. 
+
+        5. What would you do “next” to expand or augment the project?
+            
+            Answer: I would try to find more data to use/input into the data base to make my analysis more robust. If there was more data or less missing values it may have been viable to run a multiple linear regression on all the years together instea dof doing it seperately. Also, I would try to run a multiple linear 
+            regression for each course instead of clustering them. The clustering analysis may not be a perfect fit based on the variability of the courses. 
+    ''')
 
 
 
